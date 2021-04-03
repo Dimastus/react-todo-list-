4bf23cb33258ea9611457b009fd9d033bb192f3c
Dimastus <dim56026257@yandex.ru>
2021-04-04 00:56:49 +0300

    feat: move files of directory 'build' for hosting github

---
+++ precache-manifest.cf7c53f28e459fa9f20012f867b94036.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3917f0e48b49525fc196",
+    "url": "/static/css/main.a01b4a60.chunk.css"
+  },
+  {
+    "revision": "3917f0e48b49525fc196",
+    "url": "/static/js/main.3917f0e4.chunk.js"
+  },
+  {
+    "revision": "bc951937f3a44b7747ac",
+    "url": "/static/js/1.bc951937.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "295bd9990a20dbd3da2d7b31e427ead4",
+    "url": "/index.html"
+  }
+];
